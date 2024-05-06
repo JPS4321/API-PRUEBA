d9b8c74627bf3d62b479a56fb10db35d70e5f869
JPS4321 <sol22102@uvg.edu.gt>
2024-05-05 22:59:10 -0600

    d

--- conn.js
+++ conn.js
@@ -6,7 +6,8 @@ const connectionData = {
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: 5432
+    port: process.env.DB_PORT
+
 }
 
 const client = new Client(connectionData);
