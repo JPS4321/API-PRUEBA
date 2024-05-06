d5f599fcb4010fc6172a8a2fdaa4d1df85d573ca
JPS4321 <sol22102@uvg.edu.gt>
2024-05-05 23:20:25 -0600

    conn cha

--- conn.js
+++ conn.js
@@ -1,18 +1,18 @@
 import pg from 'pg';
 const { Client } = pg;
 
- 
 const connectionData = {
     user: "blog_db_p3hh_user",
-    host: "postgres://blog_db_p3hh_user:huca7Whkwl9g83KPwThvGVLigEkOM7Ry@dpg-cos39e20si5c739okhm0-a.oregon-postgres.render.com/blog_db_p3hh",
+    host: "dpg-cos39e20si5c739okhm0-a.oregon-postgres.render.com",
     database: "blog_db_p3hh",
     password: "huca7Whkwl9g83KPwThvGVLigEkOM7Ry",
-    port: 5432,
+    port: 5432
+};
 
-}
-
-console.log("Conexión a la base de datos con:", connectionData);
 const client = new Client(connectionData);
-await client.connect();
+
+client.connect().catch(error => {
+    console.error('Error al conectar a la base de datos:', error);
+});
 
 export default client;
