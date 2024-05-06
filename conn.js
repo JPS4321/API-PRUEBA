f5a42ced66e1c228efbc508a9ed30aada5881ecd
JPS4321 <sol22102@uvg.edu.gt>
2024-05-05 23:26:58 -0600

    sll

--- conn.js
+++ conn.js
@@ -6,13 +6,18 @@ const connectionData = {
     host: "dpg-cos39e20si5c739okhm0-a.oregon-postgres.render.com",
     database: "blog_db_p3hh",
     password: "huca7Whkwl9g83KPwThvGVLigEkOM7Ry",
-    port: 5432
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false // No rechazar certificados no autorizados
+    }
 };
 
 const client = new Client(connectionData);
 
-client.connect().catch(error => {
-    console.error('Error al conectar a la base de datos:', error);
-});
+client.connect()
+    .then(() => console.log("Conexión a la base de datos exitosa."))
+    .catch(error => {
+        console.error('Error al conectar a la base de datos:', error);
+    });
 
 export default client;
